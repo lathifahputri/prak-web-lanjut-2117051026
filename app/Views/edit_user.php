a064d0e0ab1df389e8b607e8b186b2e8ba993a05
Lathifah <lathifah.putri21@students.unila.ac.id>
2023-10-28 20:52:51 +0700

    utp

--- app/Views/edit_user.php
+++ app/Views/edit_user.php
@@ -1,26 +1,34 @@
 <?= $this->extend('layouts/app') ?>
 
 <?= $this->section('content') ?>
-<div class="bg">
-    <div class="container">
+<div class="container"><br>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?= base_url('/user')?>">Data Mahasiswa</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Edit Data Mahasiswa</li>
+            </ol>
+    </nav>
         <div class="row justify-content-center">
             <div class="col-lg-5">
                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Edit User</h3></div>
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Edit Data Mahasiswa</h3></div>
                     <div class="card-body">
                         <form action="<?= base_url('/user/' . $user['id']) ?>" method="POST" enctype="multipart/form-data">
                         <?= csrf_field() ?>
                         <input type="hidden" name="_method" value="PUT">
+                            <div class="wrap-foto">
+                                <img src="<?= $user['foto'] ?? '<default_foto>' ?>" class="img-fluid"> <br>
+                            </div>
                             <div class="input-group mb-3">
-                                <input type="file" class="form-control" name="foto" id="inputGroupFile02" placeholder="Upload File"><br>
-                                <img src="<?= $user['foto'] ?? '<default_foto>' ?>" class="img-fluid">
+                                <input type="file" class="form-control" name="foto" id="inputGroupFile02" placeholder="Upload File">
                             </div>
                             <div class="form-floating mb-3">
                                 <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" value="<?= $user['nama'] ?>">
                                 <label>Nama</label>
                             </div>
-                            <div class="form-floating mb-3">
-                                <select name="kelas" id="kelas" class="form-control" placeholder="Kelas" type="text">
+                            <div class="mb-3">
+                                <!-- <label class="form-label">Kelas</label> -->
+                                <select name="kelas" id="kelas" class="form-select pt-3 pb-3" type="text">
                                     <?php foreach ($kelas as $item){
                                         ?>
                                         <option value="<?= $item['id'] ?>" <?= $user['id_kelas'] == $item['id']? 'selected' : '' ?> >
@@ -36,7 +44,7 @@
                                 <label>NPM</label>
                             </div><br>
                             <div class="col-12">
-                                <button type="submit" class="btn btn-dark">Submit</button>
+                                <button type="submit" class="btn btn-dark">Simpan</button>
                             </div>
                         </form>
                     </div>
--- app/Views/list_user.php
+++ app/Views/list_user.php
@@ -8,46 +8,51 @@
             </div>
             <div class="card-body">
                 <form action="<?= base_url('user/create') ?>" method="POST">
-                    <button class="btn btn-warning">Tambah Data</button>
+                    <button class="btn btn-primary mb-3">Tambah Data</button>
                 </form>
-                <table cellpadding="10" class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Nama</th>
-                            <th>NPM</th>
-                            <th>Kelas</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        foreach ($users as $user){
-                        ?>
-                        <tr>
-                            <td><?= $user['nama'] ?></td>
-                            <td><?= $user['npm'] ?></td>
-                            <td><?= $user['nama_kelas'] ?></td>
-                            <td>
-                                <div class="wrap">
-                                    <button type="button" class="btn btn-secondary me-2"><i class="bi bi-info-circle"></i>
+                <div class="table-responsive">
+                    <table class="table table-striped" id="dataTable" width="100%"cellpadding="10">
+                        <thead>
+                            <tr>
+                                <th>No. </th>
+                                <th>Nama</th>
+                                <th>NPM</th>
+                                <th>Kelas</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            foreach ($users as $user){
+                                ?>
+                            <tr>
+                                <td><?= $no++ ?></td>
+                                <td><?= $user['nama'] ?></td>
+                                <td><?= $user['npm'] ?></td>
+                                <td><?= $user['nama_kelas'] ?></td>
+                                <td>
+                                    <div class="wrap">
+                                        <button type="button" class="btn btn-secondary me-2 detail"><i class="bi bi-info-circle"></i>
                                         <a href="<?= base_url('user/' . $user['id']) ?>"> Detail</a>
                                     </button>
-                                    <button type="button" class="btn btn-success me-2 edit" ><i class="bi bi-pencil-square"></i>
+                                        <button type="button" class="btn btn-success me-2 edit" ><i class="bi bi-pencil-square"></i>
                                         <a href="<?= base_url('/user/' . $user['id'] . '/edit') ?>"> Edit</a>
                                     </button>
                                     <form action="<?= base_url('user/' . $user['id']) ?>" method="POST">
                                         <input type="hidden" name="_method" value="DELETE">
-                                        <?= csrf_field() ?>
-                                        <button type="submit" class="btn btn-danger"><i class="bi bi-trash3"></i>Delete</button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                        <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                                            <?= csrf_field() ?>
+                                            <button type="submit" class="btn btn-danger"><i class="bi bi-trash3"></i> Hapus</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             </div> 
         </div>
     </div>
